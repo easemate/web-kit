624483640aea57258ba883fcdac65458c9bed9ba
Aaron Iker <aaron@ease.dev>
2025-12-24 03:07:14 +0100

    core utils export

---
+++ packages/core/src/utils/index.ts
@@ -0,0 +1,14 @@
+export { DismissController } from './dismiss-controller';
+export { createOutsideClickHandle } from './outside-click';
+export {
+  classMap,
+  ifDefined,
+  optionalAttribute,
+  renderIf,
+  renderList,
+  repeat,
+  styleMap,
+  styleObject,
+  unsafeHTML,
+  when
+} from './template-helpers';
